8d3e323aa47958dcaa214bf5835f09ff5ec8c9f4
Li Ge <lge@ucsd.edu>
2013-01-29 02:18:21 -0800

    Test basic case

--- tests/basic.js
+++ tests/basic.js
@@ -1,8 +1,8 @@
 var gmsh = require('../index.js');
-
+var fs = require('fs');
 // t1.geo
 var t1 = [
-  'lc = 2.5e-1;',
+  'lc = 1e-2;',
   'Point(1) = {0, 0, 0, lc};',
   'Point(2) = {.1, 0,  0, lc} ;',
   'Point(3) = {.1, .3, 0, lc} ;',
@@ -21,9 +21,12 @@ var t1 = [
   'Recombine Surface {6};'
 ].join('\n');
 
+// var t1 = fs.readFileSync(__dirname + '/t1.geo', 'utf8');
+
 var m = gmsh(t1, 'geo')
   .onOutStream('data', function(data) {
-    console.log('Data Stream:', data.toString());
+    // console.log('Data Stream:', data.toString());
+    console.log(data.toString());
   })
   // .dimension(3)
   // .outputFormat('msh')
